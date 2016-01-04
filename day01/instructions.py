b2836ec2a156cbc098e5da9a31e52940c38b8525
Rüdiger Schulz <rueschu@gmail.com>
2016-01-04 22:32:08 +0700

    execute with input data and print result

--- day01/instructions.py
+++ day01/instructions.py
@@ -9,3 +9,10 @@ def instruct(data):
     downs = total - ups
 
     return ups - downs
+
+if __name__ == '__main__':
+    fin = open("input.txt")
+    data = fin.read()
+    floor = instruct(data)
+
+    print "Santa needs to go to floor " + str(floor)

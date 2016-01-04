5159f05e3db71ba6715325ee43fb3bff6c5c04ac
Rüdiger Schulz <rueschu@gmail.com>
2016-01-04 22:07:19 +0700

    implement all other examples as tests

--- day01/instructions.test.py
+++ day01/instructions.test.py
@@ -10,6 +10,15 @@ class InstructionsTest(unittest.TestCase):
     def test_examples_with_result_three(self):
         self.assertIs(3, instructions.instruct("((("))
         self.assertIs(3, instructions.instruct("(()(()("))
+        self.assertIs(3, instructions.instruct("))((((("))
+
+    def test_examples_with_result_minus1(self):
+        self.assertIs(-1, instructions.instruct("())"))
+        self.assertIs(-1, instructions.instruct("))("))
+
+    def test_examples_with_result_minus3(self):
+        self.assertIs(-3, instructions.instruct(")))"))
+        self.assertIs(-3, instructions.instruct(")())())"))
 
 
 if __name__ == '__main__':

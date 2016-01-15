2026ffc8f1a22b8fb052e31eab6d58379c916660
Rüdiger Schulz <rueschu@gmail.com>
2016-01-15 23:31:22 +0800

    calculate both results in one go, using simple iteration

--- day01/instructions.py
+++ day01/instructions.py
@@ -1,36 +1,20 @@
 class Instructions:
     def __init__(self, data):
-        self.finalFloor = instruct(data)
-        self.firstCellar = firstcellar(data)
+        level = 0
+        steps = 0
+        while steps < len(data):
+            cmd = data[steps]
+            steps += 1
 
+            if cmd == '(':
+                level += 1
+            elif cmd == ')':
+                level -= 1
 
-def instruct(data):
-    total = len(data)
-    ups = 0
+            if level == - 1 and not hasattr(self, "firstCellar"):
+                self.firstCellar = steps
 
-    for cmd in data:
-        if cmd == '(':
-            ups += 1
-
-    downs = total - ups
-
-    return ups - downs
-
-
-def firstcellar(data):
-    level = 0
-    steps = 0
-    while steps < len(data):
-        cmd = data[steps]
-        steps += 1
-
-        if cmd == '(':
-            level += 1
-        elif cmd == ')':
-            level -= 1
-
-        if level == - 1:
-            return steps
+        self.finalFloor = level
 
 
 if __name__ == '__main__':

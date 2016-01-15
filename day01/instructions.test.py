4bd1530a94a8f61270dc0aee5185d9ee8824621f
Rüdiger Schulz <rueschu@gmail.com>
2016-01-15 22:54:05 +0800

    add test for already-computed results to tighten refactoring. use assertEquals instead of assertIs (note: find out why exactly the tests passed before, but not the one with 1795)

--- day01/instructions.test.py
+++ day01/instructions.test.py
@@ -4,25 +4,33 @@ import instructions
 
 class InstructionsTest(unittest.TestCase):
     def test_examples_with_result_zero(self):
-        self.assertIs(0, instructions.Instructions("(())").finalFloor)
-        self.assertIs(0, instructions.Instructions("()()").finalFloor)
+        self.assertEqual(0, instructions.Instructions("(())").finalFloor)
+        self.assertEqual(0, instructions.Instructions("()()").finalFloor)
 
     def test_examples_with_result_three(self):
-        self.assertIs(3, instructions.Instructions("(((").finalFloor)
-        self.assertIs(3, instructions.Instructions("(()(()(").finalFloor)
-        self.assertIs(3, instructions.Instructions("))(((((").finalFloor)
+        self.assertEqual(3, instructions.Instructions("(((").finalFloor)
+        self.assertEqual(3, instructions.Instructions("(()(()(").finalFloor)
+        self.assertEqual(3, instructions.Instructions("))(((((").finalFloor)
 
     def test_examples_with_result_minus1(self):
-        self.assertIs(-1, instructions.Instructions("())").finalFloor)
-        self.assertIs(-1, instructions.Instructions("))(").finalFloor)
+        self.assertEqual(-1, instructions.Instructions("())").finalFloor)
+        self.assertEqual(-1, instructions.Instructions("))(").finalFloor)
 
     def test_examples_with_result_minus3(self):
-        self.assertIs(-3, instructions.Instructions(")))").finalFloor)
-        self.assertIs(-3, instructions.Instructions(")())())").finalFloor)
+        self.assertEqual(-3, instructions.Instructions(")))").finalFloor)
+        self.assertEqual(-3, instructions.Instructions(")())())").finalFloor)
 
     def test_stepstocellar(self):
-        self.assertIs(1, instructions.Instructions(")").firstCellar)
-        self.assertIs(5, instructions.Instructions("()())").firstCellar)
+        self.assertEqual(1, instructions.Instructions(")").firstCellar)
+        self.assertEqual(5, instructions.Instructions("()())").firstCellar)
+
+    # added after successful run to make refactoring more secure
+    def test_expected_results(self):
+        fin = open("input.txt")
+        fullInstructions = instructions.Instructions(fin.read())
+
+        self.assertEqual(74, fullInstructions.finalFloor)
+        self.assertEqual(1795, fullInstructions.firstCellar)
 
 
 if __name__ == '__main__':

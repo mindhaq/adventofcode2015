ca5ebff6771886037e39f1a82e38e2ab60bb913e
Rüdiger Schulz <rueschu@gmail.com>
2016-01-24 15:53:49 +0800

    Day02, PHP: format

--- day02/BoxTest.class.php
+++ day02/BoxTest.class.php
@@ -12,13 +12,13 @@ class BoxTest extends PHPUnit_Framework_TestCase {
 
     public function testPaperExample1() {
         $box = new Box(2, 3, 4);
-        $this->assertEquals(58, $box->getRequiredPaper());
 
+        $this->assertEquals(58, $box->getRequiredPaper());
     }
 
     public function testPaperExample2() {
         $box = new Box(1, 1, 10);
-        $this->assertEquals(43, $box->getRequiredPaper());
 
+        $this->assertEquals(43, $box->getRequiredPaper());
     }
 }
